e18e450e82e83bb3ed9486938f477e04afb8490c
Hugo-Farias <hg80rod@gmail.com>
2023-02-25 15:35:52 -0300

    main card container completed

---
+++ src/components/Card.tsx
@@ -0,0 +1,8 @@
+import img from "../assets/image-qr-code.png";
+import cls from "./Card.module.css";
+
+const Card = function () {
+  return <div className={cls.main}></div>;
+};
+
+export default Card;
