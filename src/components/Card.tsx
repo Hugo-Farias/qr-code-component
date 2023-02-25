18f14dc4643a65174bbe236f92a946c83acf02b4
Hugo-Farias <hg80rod@gmail.com>
2023-02-25 15:52:33 -0300

    qr image added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,11 @@ import img from "../assets/image-qr-code.png";
 import cls from "./Card.module.css";
 
 const Card = function () {
-  return <div className={cls.main}></div>;
+  return (
+    <div className={cls.main}>
+      <img className={cls.image} src={img} alt="qr code image" />
+    </div>
+  );
 };
 
 export default Card;
