8ac87fc09f02c1ebec0c2d8e1ccc5ca60978fa66
Hugo-Farias <hg80rod@gmail.com>
2023-02-25 16:05:33 -0300

    text added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,13 @@ const Card = function () {
   return (
     <div className={cls.main}>
       <img className={cls.image} src={img} alt="qr code image" />
+      <div>
+        <h2>Improve your front-end skills by building projects</h2>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
     </div>
   );
 };
